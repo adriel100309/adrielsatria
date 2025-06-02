d98b620a8c4d672b437377a4dfac7133d267d628
adriel100309 <drllptp@gmail.com>
2025-06-02 12:38:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Halo Dunia")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Mari Kita Mengetahui Dunia Bersama Adriel"
 )
 st.image("IMG-20241208-WA0023.jpg", width=200)
