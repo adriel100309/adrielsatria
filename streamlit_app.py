9452546a67fc248b76635e7546930aac219314bd
adriel100309 <drllptp@gmail.com>
2025-06-02 12:32:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("Halo Dunia")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("IMG-20241208-WA0023.jpg", width=200)
